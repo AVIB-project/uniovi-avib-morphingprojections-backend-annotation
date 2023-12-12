26a21307e78d89e350718f646e612583004d56ad
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2023-12-12 17:28:10 +0100

    firts commit

---
+++ src/main/java/es/uniovi/avib/morphing/projections/backend/annotation/UnioviAvibMorphingprojectionsBackendAnnotationApplication.java
@@ -0,0 +1,13 @@
+package es.uniovi.avib.morphing.projections.backend.annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnioviAvibMorphingprojectionsBackendAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnioviAvibMorphingprojectionsBackendAnnotationApplication.class, args);
+	}
+
+}
