f243d99b4a5660c7663018b30d4a05f6e3b344c2
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2023-12-12 19:40:59 +0100

    last commit

--- src/main/java/es/uniovi/avib/morphing/projections/backend/annotation/controller/AnnotationController.java
+++ src/main/java/es/uniovi/avib/morphing/projections/backend/annotation/controller/AnnotationController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import es.uniovi.avib.morphing.projections.backend.annotation.service.Annotation
 import es.uniovi.avib.morphing.projections.backend.annotation.domain.Annotation;
 
 @Slf4j
+@CrossOrigin(maxAge = 3600)
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("annotations")
