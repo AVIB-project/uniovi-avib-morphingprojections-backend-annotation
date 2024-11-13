1dcabaf72047fda0bad2da2c8c2ceecc18440408
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2024-11-13 17:27:43 +0100

    last commit

--- src/main/java/es/uniovi/avib/morphing/projections/backend/annotation/UnioviAvibMorphingprojectionsBackendAnnotationApplication.java
+++ src/main/java/es/uniovi/avib/morphing/projections/backend/annotation/UnioviAvibMorphingprojectionsBackendAnnotationApplication.java
@@ -2,6 +2,9 @@ package es.uniovi.avib.morphing.projections.backend.annotation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class UnioviAvibMorphingprojectionsBackendAnnotationApplication {
@@ -9,4 +12,9 @@ public class UnioviAvibMorphingprojectionsBackendAnnotationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UnioviAvibMorphingprojectionsBackendAnnotationApplication.class, args);
 	}
+	
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 }
